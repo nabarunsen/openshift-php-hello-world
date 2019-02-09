83c20cd76f33f0dd4dd266057ecb70ea512ab00b
sureshchandrarhca15 <sureshchandra.rhca@gmail.com>
2019-02-09 14:08:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Hello World!</title>
 </head>
 <body>
-<?php echo "Hello World! This is the Version 3 App and Deployed by Jenkins CI Tool "; ?>
+<?php echo "Hello World! This is the Version 4 App and Deployed by Jenkins CI Tool for Pipeline Demo "; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 <?php
 }
